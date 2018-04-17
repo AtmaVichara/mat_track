6b8aacd8f9ca241b17adbd805941ba9c52c0f81d
Joe Jobes <jmrjobes@gmail.com>
2018-04-17 12:04:00 -0600

    Add #user_lessons class method to Lesson
    * Added #user_lessons which grabs all the lessons associated to a current_user

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,11 @@ class Lesson < ApplicationRecord
   has_many :events
   has_many :school_lessons
   has_many :schools, through: :school_lessons
+  has_many :attendances, through: :events
+
+  def self.users_lessons(user_id)
+    joins(schools: :user_schools)
+      .group(:id)
+      .where('user_schools.user_id = ?', user_id)
+  end
 end
