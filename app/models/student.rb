50164aac0520f65bccdb35b93e1e0c2e6705ea4e
Joe Jobes <jmrjobes@gmail.com>
2018-04-18 12:49:53 -0600

    Add dependent destroy to students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
-  has_many :user_students
+  has_many :user_students, dependent: :destroy
   has_many :users, through: :user_students
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
   has_many :events, through: :attendances
   has_many :user_schools
   has_many :schools, through: :user_schools
