3114f36d0173c385b4bbd54febb26af3aa4c4815
Joe Jobes <jmrjobes@gmail.com>
2018-04-18 07:32:28 -0600

    Add api routes and events collection
    * Added events collction :add_attendee as a post route
    * Added api routes for events and attendees dealing with events

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,21 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "/dashbord", to: 'users#show', as: 'dashboard'
 
+  namespace :api do
+    namespace :v1 do
+      resources :events, only: [:index, :show], module: :events do
+        get '/attendees', to: 'attendees#show'
+      end
+    end
+  end
+
   resources :users, only: [:new, :create]
   resources :lessons
-  resources :events
+  resources :events do
+    collection do
+      post :add_attendee, as: 'add_attendee'
+    end
+  end
   resources :schools
   resources :students, only: [:index, :create, :show]
 
